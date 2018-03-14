e9330a291d8628b56ba5008a5455e18edb20dd23
Jeremy Greenwood <jeremy@mobelux.com>
2018-03-14 10:54:43 -0400

    remove spm artifacts

---
+++ ImageLoader/ImageLoader.h
@@ -0,0 +1,19 @@
+//
+//  ImageLoader.h
+//  ImageLoader
+//
+//  Created by Jeremy Greenwood on 3/14/18.
+//  Copyright © 2018 Mobelux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageLoader.
+FOUNDATION_EXPORT double ImageLoaderVersionNumber;
+
+//! Project version string for ImageLoader.
+FOUNDATION_EXPORT const unsigned char ImageLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageLoader/PublicHeader.h>
+
+
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import ImageLoaderTests
-
-XCTMain([
-    testCase(ImageLoaderTests.allTests),
-])
