8edecc6adfba2e9653ec6b337e27109351b01232
Jeremy Greenwood <jeremy@mobelux.com>
2020-03-24 16:04:25 -0400

    added readme and license

--- README.md
+++ README.md
@@ -1,3 +1,71 @@
 # ImageFetcher
 
-A description of this package.
+ImageFetcher is lightweight image loading library build on top of `OperationQueue`. It is optimizes for scroll view performance by decompressing images in the background and exposing a configuration option for rounding corners of the image.
+
+## 📱 Requirements
+
+Swift 5.1x toolchain with Swift Package Manager, iOS 12
+
+## 🖥 Installation
+
+### 📦 Swift Package Manager (recommended)
+
+Add `ImageFetcher` to your `Packages.swift` file:
+
+```swift
+.package(url: "https://github.com/Mobelux/ImageFetcher.git", from: "1.0.0"),
+```
+
+## ⚙️ Usage
+
+Intialize `ImageFetcher` with a `Cache`:
+
+```swift
+let fetcher = try ImageFetcher(DiskCache(storageType: .temporary(nil)))
+```
+
+Optionally initialize with a queue and maximum concurrent download count:
+
+```swift
+let fetcher = try ImageFetcher(DiskCache(storageType: .temporary(nil)), queue: OperationQueue(), maxConcurrent: 5)
+```
+
+To fetch an image from the web:
+
+```swift
+let config = ImageConfiguration(url: URL(string: "https://via.placeholder.com/150")!)
+fetcher.load(config) { result in
+    switch result {
+    case .success(let imageResult):
+        let image = imageResult.value
+    default: break
+    }
+}
+```
+
+Fetch an image with configuration options and robust handling:
+
+```swift
+let config = ImageConfiguration(url: URL(string: "https://via.placeholder.com/150")!,
+                                size: CGSize(width: 100.0, height: 100.0),
+                                constrain: true,
+                                cornerRadius: 10.0,
+                                scale: 1)
+fetcher.load(config) { result in
+    switch result {
+    case .success(let imageResult):
+        switch imageResult {
+        case .cached(let image):
+            /// handle image coming from cache
+        case .downloaded(let image):
+            /// handle newly downloaded image
+        }
+    case .failure(let error):
+        /// handle error
+    }
+}
+```
+
+## License
+
+ImageFetcher is release under MIT licensing.
--- Sources/ImageFetcher/ImageFetcher.swift
+++ Sources/ImageFetcher/ImageFetcher.swift
@@ -2,8 +2,27 @@
 //  ImageFetcher.swift
 //  Mobelux
 //
-//  Created by Jeremy Greenwood on 3/13/18.
-//  Copyright © 2018 Mobelux. All rights reserved.
+//  MIT License
+//
+//  Copyright (c) 2020 Mobelux LLC
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
 import UIKit
@@ -31,7 +50,7 @@ public final class ImageFetcher: ImageFetching {
      - imageConfiguration: The configuation of the image to be installed.
      - handler: The handler which passes in an `ImageLoaderTask`
 
-     **/
+     */
     public func task(_ imageConfiguration: ImageConfiguration, handler: @escaping (ImageFetcherTask) -> ()) {
         workerQueue.async {
             // if data is cached, use it, else use `DataOperation` to fetch image data
@@ -60,7 +79,7 @@ public final class ImageFetcher: ImageFetching {
      - imageConfiguration: The configuation of the image to be installed.
      - handler: The handler which passes in an `ImageHandler`
 
-     **/
+     */
     public func load(_ imageConfiguration: ImageConfiguration, handler: ImageHandler?) {
         task(imageConfiguration) { [unowned self] task in
             if let result = task.result {
