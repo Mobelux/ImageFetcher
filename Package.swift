7c3f290b218c5c64391a708144745c3b4c88d809
Nicholas Bellucci <nickdbellucci@gmail.com>
2020-07-06 15:59:42 -0400

    Updated package dependencies to use https

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "git@github.com:Mobelux/DataOperation.git", from: "1.1.0"),
-         .package(url: "git@github.com:Mobelux/DiskCache.git", from: "1.1.0")
+         .package(url: "https://github.com/Mobelux/DataOperation", from: "1.1.0"),
+         .package(url: "https://github.com/Mobelux/DiskCache", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
