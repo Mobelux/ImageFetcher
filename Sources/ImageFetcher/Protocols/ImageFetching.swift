376fe3d0b213760d162013c22300ab5a42f9efd4
Matt Gacy <matt@mgacy.com>
2023-03-20 14:47:44 -0700

    Return Task, ImageSource; remove ImageFetcherTask

--- Sources/ImageFetcher/Protocols/ImageFetching.swift
+++ Sources/ImageFetcher/Protocols/ImageFetching.swift
@@ -13,25 +13,25 @@ import UIKit
 #endif
 
 public protocol ImageURLFetching {
-    func task(_ url: URL) async -> ImageFetcherTask
-    func load(_ url: URL) async -> ImageResult
+    func task(_ url: URL) async -> Task<ImageSource, Error>
+    func load(_ url: URL) async throws -> ImageSource
     func cancel(_ url: URL)
     func cache(_ image: Image, key: URL) async throws
     func delete(_ url: URL) async throws
     func deleteCache() async throws
 
-    subscript (_ url: URL) -> ImageFetcherTask? { get }
+    subscript (_ url: URL) -> Task<ImageSource, Error>? { get }
 }
 
 public protocol ImageConfigurationFetching {
-    func task(_ imageConfiguration: ImageConfiguration) async -> ImageFetcherTask
-    func load(_ imageConfiguration: ImageConfiguration) async -> ImageResult
+    func task(_ imageConfiguration: ImageConfiguration) async -> Task<ImageSource, Error>
+    func load(_ imageConfiguration: ImageConfiguration) async throws -> ImageSource
     func cancel(_ imageConfiguration: ImageConfiguration)
     func cache(_ image: Image, key: ImageConfiguration) async throws
     func delete(_ imageConfiguration: ImageConfiguration) async throws
     func deleteCache() async throws
 
-    subscript (_ imageConfiguration: ImageConfiguration) -> ImageFetcherTask? { get }
+    subscript (_ imageConfiguration: ImageConfiguration) -> Task<ImageSource, Error>? { get }
 }
 
 public protocol ImageFetching: ImageURLFetching & ImageConfigurationFetching {}
