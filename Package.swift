4f46bb8f8636b599f93f3b9cf011d1cb7b9d1ef6
Matt Gacy <matt@mgacy.com>
2023-12-26 16:25:28 -0700

    Add .git extension

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "ImageFetcher", targets: ["ImageFetcher"])
     ],
     dependencies: [
-         .package(url: "https://github.com/Mobelux/DiskCache", from: "2.0.0"),
+         .package(url: "https://github.com/Mobelux/DiskCache.git", from: "2.0.0"),
          .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
     ],
     targets: [
