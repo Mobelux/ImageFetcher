76ddafb45ce7fdb4f590c937ca46bcc262a2c8f9
Matt Gacy <matt@mgacy.com>
2023-12-26 15:17:07 -0700

    Clean up manifest

--- Package.swift
+++ Package.swift
@@ -12,28 +12,25 @@ let package = Package(
         .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "ImageFetcher",
-            targets: ["ImageFetcher"]),
+        .library(name: "ImageFetcher", targets: ["ImageFetcher"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/Mobelux/DiskCache", from: "2.0.0"),
          .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ImageFetcher",
             dependencies: [
                 "DiskCache",
-                .product(
-                    name: "Crypto",
-                    package: "swift-crypto")]),
+                .product(name: "Crypto", package: "swift-crypto")
+            ]
+        ),
         .testTarget(
             name: "ImageFetcherTests",
-            dependencies: ["ImageFetcher"]),
+            dependencies: [
+                "ImageFetcher"
+            ]
+        )
     ]
 )
